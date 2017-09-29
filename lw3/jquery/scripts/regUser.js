6ccd317449afb082fcfb15ac9697fdedcd2bcb52
Rekazz <programmer-2017@protonmail.ch>
2017-09-30 01:13:55 +0300

    Its Works!

---
+++ lw3/jquery/scripts/regUser.js
@@ -0,0 +1,26 @@
+function regUser() {
+  var email = $("#email").val();
+  var passwd = $("#passwd").val();
+  alert(email);
+  var confirmPasswd = $("#confirm-passwd").val();
+  var choice = document.querySelector('input[type=checkbox]');
+  if (email == "") {
+    alert("Требуется Заполнить Поле [Эл. Почта]");
+    return 0;
+  } else if (passwd == "" || passwd.length < 6) {
+    alert("Требуется Заполнить Поле [Пароль]  Мин = 6 Символов");
+    return 0;
+  } else if (confirmPasswd == "" || confirmPasswd < 6) {
+    alert("Требуется Заполнить Поле [Повтор Пароля] Мин = 6 Символов");
+    return 0;
+  } else if (choice.checked != true) {
+    alert("Требуется Поставить Флажок В Поле [Согласен?]")
+    return 0;
+  }
+  if (passwd != confirmPasswd) {
+    alert("Ошибка, Введенные Пароли Не Совпадают!");
+    return 0;
+  } else {
+    confirm("Регистрация Пользователя Завершена!");
+  }
+};
